3a321dacfcfe412510d5400d7aefdec9204c5dd9
qq987985 <76234344+qq987985@users.noreply.github.com>
2026-08-28 16:11:44 +0800

    Add files via upload

--- win/启动.bat
+++ win/启动.bat
@@ -27,6 +27,7 @@ mkdir "data\photos\收款回单" 2>nul
 mkdir "data\photos\考勤影像" 2>nul
 mkdir "data\photos\合同扫描件" 2>nul
 mkdir "data\photos\报销凭证" 2>nul
+mkdir "data\photos\报销打款" 2>nul
 
 set "GONGDI_HOME=%~dp0"
 set GONGDI_PORTABLE=1
